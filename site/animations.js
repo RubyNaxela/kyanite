ceaa01c2d307da67ce2170969ec65337b8ae6fab
RubyNaxela <rubynaxela@gmail.com>
2021-11-01 12:59:43 +0100

    Updates on the website

--- site/animations.js
+++ site/animations.js
@@ -1,16 +1,19 @@
 (() => {
 
-    const fadeElements = document.querySelectorAll(".fade-in");
     const rem = parseFloat(getComputedStyle(document.documentElement).fontSize);
 
-    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+    const fade = (entries, appearOnScroll) => {
         entries.filter(entry => entry.isIntersecting).forEach(entry => {
             entry.target.classList.add("appearred");
             appearOnScroll.unobserve(entry.target);
         });
-    }, {threshold: 1, rootMargin: `0px 0px -${10 * rem}px 0px`});
+    };
 
-    fadeElements.forEach(element => appearOnScroll.observe(element));
+    const appearOnScroll = new IntersectionObserver(fade, {threshold: 1});
+    const appearOnScrollEarly = new IntersectionObserver(fade, {threshold: 0, rootMargin: `0px 0px -${10 * rem}px 0px`});
+
+    document.querySelectorAll(".fade-in").forEach(element => appearOnScroll.observe(element));
+    document.querySelectorAll(".fade-in-early").forEach(element => appearOnScrollEarly.observe(element));
 
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
