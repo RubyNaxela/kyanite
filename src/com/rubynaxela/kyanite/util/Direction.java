026cf2bb2a9f49108911069d70fb0e48fd4457c7
RubyNaxela <rubynaxela@gmail.com>
2021-11-07 00:14:53 +0100

    [pre-1.6.1] Added the Direction enum

---
+++ src/com/rubynaxela/kyanite/util/Direction.java
@@ -0,0 +1,8 @@
+package com.rubynaxela.kyanite.util;
+
+/**
+ * A simple enum for representing the four cardinal and four intercardinal directions on a two-dimensional plane.
+ */
+public enum Direction {
+    NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST
+}
