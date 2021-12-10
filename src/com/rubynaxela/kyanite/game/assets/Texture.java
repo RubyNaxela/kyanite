e2fe335d89a642f887927f0d6151ed38658298b8
RubyNaxela <rubynaxela@gmail.com>
2021-12-10 19:22:10 +0100

    [1.7.1] If image data source for a Texture is not valid, the texture is replaced with a magenta-black checkboard

--- src/com/rubynaxela/kyanite/game/assets/Texture.java
+++ src/com/rubynaxela/kyanite/game/assets/Texture.java
@@ -2,6 +2,8 @@ package com.rubynaxela.kyanite.game.assets;
 
 import com.rubynaxela.kyanite.system.IOException;
 import org.jetbrains.annotations.NotNull;
+import org.jsfml.graphics.FloatRect;
+import org.jsfml.graphics.IntRect;
 import org.jsfml.graphics.Shape;
 import org.jsfml.graphics.Sprite;
 
@@ -9,14 +11,18 @@ import java.io.File;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 /**
  * A wrapper class for JSFML {@link org.jsfml.graphics.Texture} objects, representing a texture asset.
- * The source must be the path to an image file. Supported formats are: BMP, DDS, JPEG, PNG, TGA and PSD.
+ * The source must be the path or the stream to an image file. Supported formats are: BMP, DDS, JPEG,
+ * PNG, TGA and PSD. If the specified image data source is not valid (for instance, if the specified
+ * file does not exist), the texture is replaced with a magenta-black checkboard.
  */
 public class Texture implements Asset {
 
     private final org.jsfml.graphics.Texture texture;
+    private boolean missing = false;
 
     Texture(@NotNull org.jsfml.graphics.Texture texture) {
         this.texture = texture;
@@ -32,7 +38,8 @@ public class Texture implements Asset {
         try {
             texture.loadFromFile(path);
         } catch (java.io.IOException e) {
-            throw new IOException(e);
+            e.printStackTrace();
+            missingTexture();
         }
     }
 
@@ -64,26 +71,35 @@ public class Texture implements Asset {
         try {
             texture.loadFromStream(stream);
         } catch (java.io.IOException e) {
-            throw new IOException(e);
+            e.printStackTrace();
+            missingTexture();
         }
     }
 
     /**
-     * Applies this texture on the {@link Shape} without affecting the texture rectangle.
+     * Applies this texture on the {@link Shape}. Does not affect the texture rectangle, unless the texture is missing.
      *
      * @param shape the {@link Shape} to apply this texture on
      */
     public void apply(@NotNull Shape shape) {
         shape.setTexture(texture);
+        if (missing) {
+            final FloatRect bounds = shape.getLocalBounds();
+            shape.setTextureRect(new IntRect(0, 0, (int) bounds.width, (int) bounds.height));
+        }
     }
 
     /**
-     * Applies this texture on the {@link Sprite} without affecting the texture rectangle.
+     * Applies this texture on the {@link Sprite}. Does not affect the texture rectangle, unless the texture is missing.
      *
      * @param sprite the {@link Sprite} to apply this texture on
      */
     public void apply(@NotNull Sprite sprite) {
         sprite.setTexture(texture);
+        if (missing) {
+            final FloatRect bounds = sprite.getLocalBounds();
+            sprite.setTextureRect(new IntRect(0, 0, (int) bounds.width, (int) bounds.height));
+        }
     }
 
     /**
@@ -99,7 +115,7 @@ public class Texture implements Asset {
      * @param tileable true to enable, false to disable
      */
     public void setTileable(boolean tileable) {
-        texture.setRepeated(true);
+        texture.setRepeated(tileable);
     }
 
     /**
@@ -116,5 +132,15 @@ public class Texture implements Asset {
      */
     public void setSmooth(boolean smooth) {
         texture.setSmooth(smooth);
+    }
+
+    private void missingTexture() {
+        try {
+            missing = true;
+            texture.loadFromStream(Objects.requireNonNull(getClass().getResourceAsStream("/res/missing_texture.png")));
+            texture.setRepeated(true);
+        } catch (java.io.IOException ex) {
+            throw new IOException(ex);
+        }
     }
 }
